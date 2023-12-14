ea2a35c4b21b5d32feb8cbe8a9926959c1632d4b
testuser-at-490752553772 <m_aronis@hotmail.com>
2023-12-14 23:17:39 +0200

    fix go versionin runner, modify ssm command in deployment script

--- .github/scripts/deploy_testnet.sh
+++ .github/scripts/deploy_testnet.sh
@@ -7,7 +7,7 @@ fetch_binary () {
       aws ssm send-command \
     --instance-ids $instance \
     --document-name "AWS-RunShellScript" \
-    --parameters commands="aws s3 cp://evmosd-binaries/$BINARY /home/ubuntu/go/bin/evmosd"
+    --parameters commands="rm /home/ubuntu/go/bin/evmosd;aws s3 cp://evmosd-binaries/$BINARY /home/ubuntu/go/bin/evmosd;sudo chown 1000:1000 /home/ubuntu/go/bin/evmosd"
 }
 
 get_instance_id () {
